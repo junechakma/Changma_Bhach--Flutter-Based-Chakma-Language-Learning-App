76e7cb1cee980dc6b41d55d63f7bec59b80ccf8d
junechakma <junechakma50@gmail.com>
2024-11-19 20:01:03 +0600

    Swipe Left in Lesson Screen

--- lib/presentation/screens/lessons/lesson_screen.dart
+++ lib/presentation/screens/lessons/lesson_screen.dart
@@ -97,149 +97,158 @@ class _LessonScreenState extends State<LessonScreen> {
         }
         ExitAlert.show(context);
       },
-      child: Scaffold(
-        backgroundColor: AppColors.backgroundColor,
-        appBar: AppBar(
+      child: GestureDetector(
+        onHorizontalDragEnd: (DragEndDetails details) {
+          if (details.primaryVelocity! < 0) {
+            // Negative velocity means left swipe
+            Navigator.pushNamed(context, AppRoutes.drawingScreen);
+          }
+        },
+        child: Scaffold(
           backgroundColor: AppColors.backgroundColor,
-          title: Text(
-            appBarTitle == "vowel"
-                ? AppLocalizations.of(context)!.vowel
-                : appBarTitle == "diacritics"
-                    ? AppLocalizations.of(context)!.diacritics
-                    : appBarTitle == "numbers"
-                        ? AppLocalizations.of(context)!.numerals
-                        : AppLocalizations.of(context)!.consonant,
-            style: TextStyles.lessonText.copyWith(fontSize: 26),
+          appBar: AppBar(
+            backgroundColor: AppColors.backgroundColor,
+            title: Text(
+              appBarTitle == "vowel"
+                  ? AppLocalizations.of(context)!.vowel
+                  : appBarTitle == "diacritics"
+                      ? AppLocalizations.of(context)!.diacritics
+                      : appBarTitle == "numbers"
+                          ? AppLocalizations.of(context)!.numerals
+                          : AppLocalizations.of(context)!.consonant,
+              style: TextStyles.lessonText.copyWith(fontSize: 26),
+            ),
+            centerTitle: true,
+            actions: const [
+              ScoreCounter(),
+              SizedBox(
+                width: 20,
+              )
+            ],
           ),
-          centerTitle: true,
-          actions: const [
-            ScoreCounter(),
-            SizedBox(
-              width: 20,
-            )
-          ],
-        ),
-        body: Container(
-          margin: const EdgeInsets.only(top: 20),
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                height: 400,
-                padding: const EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: AppColors.lessonBg),
-                child: Center(
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          IconButton(
-                              onPressed: () {
-                                final audioPath =
-                                    lessonProvider.content['audio'] as String?;
-                                _playAudio('audios/congratulations.mp3');
-                              },
-                              icon: const Icon(
-                                Icons.volume_up,
-                                size: 32,
-                              ))
-                        ],
-                      ),
-                      Text(
-                        lessonProvider.content["letter"] ?? '',
-                        style:
-                            TextStyles.categoryHeading.copyWith(fontSize: 120),
-                      ),
-                      Text(
-                        lessonProvider.content["pronunciation"] ?? '',
-                        style: TextStyles.subHeadingText
-                            .copyWith(fontSize: 26, color: AppColors.primary),
-                      ),
-                      const SizedBox(
-                        height: 50,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "শব্দঃ",
-                            style: TextStyles.subHeadingText.copyWith(
-                                fontSize: 18, color: AppColors.primary),
-                          ),
-                          const Text(
-                            " ",
-                          ),
-                          Text(
-                            lessonProvider.content["word"] ?? '',
-                            style: TextStyles.subHeadingText.copyWith(
-                                fontSize: 18, color: AppColors.darkLight),
-                          ),
-                          Text(
-                            " >> ",
-                            style: TextStyles.subHeadingText.copyWith(
-                                fontSize: 18, color: AppColors.darkLight),
-                          ),
-                          Text(
-                            lessonProvider.content["chakmaWord"] ?? '',
-                            style: TextStyles.subHeadingText.copyWith(
-                                fontSize: 18, color: AppColors.darkLight),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Text(
-                lessonProvider.content["rules"] ?? '',
-                style:
-                    TextStyles.lessonText.copyWith(color: AppColors.darkLight),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, AppRoutes.drawingScreen);
-                    },
-                    style: ButtonStyle(
-                        foregroundColor:
-                            WidgetStateProperty.all(AppColors.backgroundColor),
-                        backgroundColor:
-                            WidgetStateProperty.all(AppColors.primary)),
-                    child: Row(
+          body: Container(
+            margin: const EdgeInsets.only(top: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 400,
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: AppColors.lessonBg),
+                  child: Center(
+                    child: Column(
                       children: [
-                        Text(AppLocalizations.of(context)!.draw_button),
-                        const FaIcon(FontAwesomeIcons.paintbrush)
-                      ], // Removed `Flexible`
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            IconButton(
+                                onPressed: () {
+                                  final audioPath = lessonProvider
+                                      .content['audio'] as String?;
+                                  _playAudio('audios/congratulations.mp3');
+                                },
+                                icon: const Icon(
+                                  Icons.volume_up,
+                                  size: 32,
+                                ))
+                          ],
+                        ),
+                        Text(
+                          lessonProvider.content["letter"] ?? '',
+                          style: TextStyles.categoryHeading
+                              .copyWith(fontSize: 120),
+                        ),
+                        Text(
+                          lessonProvider.content["pronunciation"] ?? '',
+                          style: TextStyles.subHeadingText
+                              .copyWith(fontSize: 26, color: AppColors.primary),
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "শব্দঃ",
+                              style: TextStyles.subHeadingText.copyWith(
+                                  fontSize: 18, color: AppColors.primary),
+                            ),
+                            const Text(
+                              " ",
+                            ),
+                            Text(
+                              lessonProvider.content["word"] ?? '',
+                              style: TextStyles.subHeadingText.copyWith(
+                                  fontSize: 18, color: AppColors.darkLight),
+                            ),
+                            Text(
+                              " >> ",
+                              style: TextStyles.subHeadingText.copyWith(
+                                  fontSize: 18, color: AppColors.darkLight),
+                            ),
+                            Text(
+                              lessonProvider.content["chakmaWord"] ?? '',
+                              style: TextStyles.subHeadingText.copyWith(
+                                  fontSize: 18, color: AppColors.darkLight),
+                            ),
+                          ],
+                        )
+                      ],
                     ),
                   ),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              LinearProgressIndicator(
-                value: lessonProvider.lessonProgress,
-                valueColor: const AlwaysStoppedAnimation(AppColors.quinaryDark),
-                backgroundColor: AppColors.quinaryLight,
-                borderRadius: BorderRadius.circular(8),
-                minHeight: 10,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-            ],
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  lessonProvider.content["rules"] ?? '',
+                  style: TextStyles.lessonText
+                      .copyWith(color: AppColors.darkLight),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, AppRoutes.drawingScreen);
+                      },
+                      style: ButtonStyle(
+                          foregroundColor: WidgetStateProperty.all(
+                              AppColors.backgroundColor),
+                          backgroundColor:
+                              WidgetStateProperty.all(AppColors.primary)),
+                      child: Row(
+                        children: [
+                          Text(AppLocalizations.of(context)!.draw_button),
+                          const FaIcon(FontAwesomeIcons.paintbrush)
+                        ], // Removed `Flexible`
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                LinearProgressIndicator(
+                  value: lessonProvider.lessonProgress,
+                  valueColor:
+                      const AlwaysStoppedAnimation(AppColors.quinaryDark),
+                  backgroundColor: AppColors.quinaryLight,
+                  borderRadius: BorderRadius.circular(8),
+                  minHeight: 10,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
           ),
         ),
       ),
